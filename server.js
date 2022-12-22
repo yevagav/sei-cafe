728118f65d2e874c969c92c025864262e3af7e48
yevagav <yevagavrilov@gmail.com>
2022-12-21 16:26:24 -0800

    first commit

--- server.js
+++ server.js
@@ -1,24 +1,37 @@
 require('dotenv').config()
+require('./config/database');
 const express = require('express')
 const path = require('path')
 const favicon = require('serve-favicon')
 const logger = require('morgan')
 const PORT = process.env.PORT || 3001
 
-const app = express() 
+const app = express()
 
-app.use(express.json()) // req.body
-app.use(logger('dev'))
-app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
-app.use(express.static(path.join(__dirname, 'build')))
-
-/* 
-app.use('/api, routes) <======= Finish code once you got it
-*/
-app.get('/api/test', (req,res) => {
-    res.json({'eureka': 'you have found it'})
+app.use(express.json())// req.body
+app.use((req, res, next) => {
+    res.locals.data = {}
+    next()
 })
 
+app.use(logger('dev'))
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico' )))
+app.use(express.static(path.join(__dirname, 'build')))
+
+// Check if token and create req.user
+app.use(require('./config/checkToken'))
+
+/*
+app.use('/api', routes) <====== Finish code once you got it
+*/
+
+// Put API routes here, before the "catch all" route
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/fruits', require('./routes/api/fruits'))
+
+app.get('/api/test', (req, res) => {
+    res.json({'eureka': 'you have found it'})
+})
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
@@ -27,4 +40,3 @@ app.get('*', (req, res) => {
 app.listen(PORT, () => {
     console.log(`I am listening on ${PORT}`)
 })
-
